f9e6b96084e5cf430457ed63b5db032b7ada6051
monseroad <monsepino@gmail.com>
2017-01-07 14:03:25 -0300

    falta aproximadamente 20% por terminar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,42 @@
+//MENÚ
 $(window).load(function() {
   $(".btn-nav").on("click tap", function() {
     $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
     $(this).toggleClass("animated");
   });
 });
+
+//SMOOVE
+
+//SMOOVE SECCION 2 
+$(document).ready(function(){
+  $(".foto-dos-cuatro").smoove({ //FOTO 2 y 4
+    offset  : '10%',
+    moveX   : '100px',
+    moveY   : '100px',
+
+  });  
+
+  $(".foto-tres").smoove({ //FOTO 3
+    offset  : '10%',
+    moveX   : '50px',
+    moveY   : '250px',
+
+  });  
+});
+
+
+//SMOOVE SECCION 5
+$(".foo, .bar").smoove({
+    skew  : '15%',
+    moveX   : '100px',
+    moveY   : '100px',
+  }); 
+
+//SLIDE
+$(function() {
+      $('#slides').superslides({
+        hashchange: true
+      });
+    });
+
